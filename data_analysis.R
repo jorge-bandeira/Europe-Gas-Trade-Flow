4f086c15264f86708237a7723ea0a63997c4982d
IP93 <invictaproject1893@gmail.com>
2022-07-29 22:08:09 +0100

    changed cleaning script to separate month and year columns

--- data_analysis.R
+++ data_analysis.R
@@ -1,4 +1,10 @@
 library(tidyverse)
+library(ggplot2)
 
 #import clean data
 gas_data <- read_csv("gas_exports_data_cleaned.csv")
+
+#check top exporters
+
+
+#plot exports
--- data_cleaning.R
+++ data_cleaning.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(readxl)
 library(data.table)
 library(lubridate)
-library(zoo)
 
 # read raw xls file
 raw_df <- read_excel("Export_GTF_IEA_202205_edited.xls")
@@ -13,14 +12,15 @@ unp_df <- melt(setDT(raw_df), id.vars = c("Borderpoint", "Exit", "Entry"), varia
 #clean col names
 colnames(unp_df)[5] <- "gtf_Mcm"
 
-#convert month column to mm-yyyy
+#create year and month columns
 unp_df <- unp_df %>%
   transform(Month = as.numeric(as.character(Month))) %>% 
-  mutate(Month = as_date(Month, origin = date("1900/01/01"))) %>% 
-  mutate(Month = as.yearmon(Month))
+  mutate(date = as_date(Month, origin = date("1900/01/01"))) %>% 
+  mutate(month = month(date, label = TRUE, abbr = TRUE)) %>% 
+  mutate(year = year(date))
 
 #change NA values to 0, make gtf column numeric
-unp_df[unp_df$gtf_Mcm == "N/A"] <- 0
+unp_df$gtf_Mcm[unp_df$gtf_Mcm == "N/A"] <- 0
 unp_df <- unp_df %>% 
   mutate(gtf_Mcm = as.numeric(gtf_Mcm))
 
