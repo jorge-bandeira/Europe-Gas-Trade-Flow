548570c4efe2a247790544238d70646fda2f3026
IP93 <invictaproject1893@gmail.com>
2022-08-01 22:57:51 +0100

    limited chart to flows > 5000, changed visual properties

--- data_analysis.R
+++ data_analysis.R
@@ -12,25 +12,19 @@ getTrade <- function(f_year) {
     group_by(exit, entry) %>% 
     summarise(export = sum(gtf_mcm)) %>% 
     arrange(desc(export)) %>% 
-    filter(export > 0)
+    filter(export > 2000)
 }
 
-top_exp_2021 <- getTrade(2021)
 top_exp_2022 <- getTrade(2022)
 
 #create nodes df for networkD3
-nodes_2021 <- data.frame(name=c(as.character(top_exp_2021$exit), as.character(top_exp_2021$entry)) %>% unique())
 nodes_2022 <- data.frame(name=c(as.character(top_exp_2022$exit), as.character(top_exp_2022$entry)) %>% unique())
 
 #set ids for networkD3
-top_exp_2021$exitId = match(top_exp_2021$exit, nodes_2021$name) - 1
-top_exp_2021$entryId = match(top_exp_2021$entry, nodes_2021$name) - 1
 top_exp_2022$exitId = match(top_exp_2022$exit, nodes_2022$name) - 1
 top_exp_2022$entryId = match(top_exp_2022$entry, nodes_2022$name) - 1
 
 #create chat with overview of natural gas trade
-gas_trade_chart_2021 <- sankeyNetwork(Links = top_exp_2021, Nodes = nodes_2021, Source = "exitId", Target = "entryId", 
-                                 Value = "export", NodeID = "name")
-
 gas_trade_chart_2022 <- sankeyNetwork(Links = top_exp_2022, Nodes = nodes_2022, Source = "exitId", Target = "entryId", 
-                                      Value = "export", NodeID = "name") 
+                                      Value = "export", NodeID = "name", sinksRight = TRUE, fontFamily = "Arial", fontSize = 10, nodePadding = 20)
+
